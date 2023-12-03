421fc8eb95507b727ce013d9c9fada6260f17ce9
Skye <Mourad.rea@gmail.com>
2023-12-02 23:42:28 -0500

    Enemy follows the player

--- Source/FighterGame/FighterGameGameMode.cpp
+++ Source/FighterGame/FighterGameGameMode.cpp
@@ -4,6 +4,7 @@
 #include "FighterGameCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 #include <Kismet/GameplayStatics.h>
+#include "GameFramework/CharacterMovementComponent.h"
 #include "BaseEnemy.h"
 #include "FloatingActor.h"
 
@@ -24,7 +25,7 @@ void
 AFighterGameGameMode::BeginPlay()
 {
    Spawn();
-   GetWorldTimerManager().SetTimer(SpawnTimerHandle, this, &AFighterGameGameMode::Spawn, 5.0f, true);
+   GetWorldTimerManager().SetTimer(SpawnTimerHandle, this, &AFighterGameGameMode::Spawn, 10.0f, true);
 }
 
 void
@@ -55,42 +56,28 @@ AFighterGameGameMode::Spawn()
 
             // Calculate a random spawn location within the radius
             FVector2D SpawnLocation2D = FMath::RandPointInCircle(SpawnRadius);
-            SpawnLocation
-               = FVector(SpawnLocation2D.X, SpawnLocation2D.Y, 0.0f)
-                 + PlayerPawn->GetActorLocation();
+            SpawnLocation = FVector(SpawnLocation2D.X, SpawnLocation2D.Y, 0.0f) + PlayerPawn->GetActorLocation();
 
             // Initialize HitResult
             FHitResult HitResult;
 
             // Find the floor at the spawn location
-            if(GetWorld()->SweepSingleByChannel(
-                  HitResult, SpawnLocation,
-                  SpawnLocation - FVector(0, 0, 1000), FQuat::Identity,
-                  ECC_Visibility, FCollisionShape::MakeSphere(50),
-                  CollisionParams))
+            if(GetWorld()->SweepSingleByChannel(HitResult, SpawnLocation, SpawnLocation - FVector(0, 0, 1000), FQuat::Identity, ECC_Visibility, FCollisionShape::MakeSphere(50), CollisionParams))
                {
                   // Set the spawn location to the floor location
-                  SpawnLocation.Z
-                     = HitResult.ImpactPoint.Z + 50.0f; // Adjust as needed
+                  SpawnLocation.Z = HitResult.ImpactPoint.Z + 50.0f; // Adjust as needed
                }
 
             // Check if colliding
             CollisionParams.AddIgnoredActor(PlayerPawn);
          }
-      while(GetWorld()->LineTraceTestByChannel(PlayerPawn->GetActorLocation(),
-                                               SpawnLocation, ECC_Visibility,
-                                               CollisionParams));
+      while(GetWorld()->LineTraceTestByChannel(PlayerPawn->GetActorLocation(),  SpawnLocation, ECC_Visibility, CollisionParams));
 
-      if(!GetWorld()->LineTraceTestByChannel(PlayerPawn->GetActorLocation(),
-                                             SpawnLocation, ECC_Visibility,
-                                             CollisionParams))
+      if(!GetWorld()->LineTraceTestByChannel(PlayerPawn->GetActorLocation(), SpawnLocation, ECC_Visibility, CollisionParams))
          {
             // Spawn the creature
-            ABaseEnemy *NewCreature = GetWorld()->SpawnActor<ABaseEnemy>(
-               baseEnemy, SpawnLocation, FRotator::ZeroRotator);
-            UE_LOG(LogTemp, Warning,
-                   TEXT("Creature spawned at X: %f, Y: %f, Z: %f"),
-                   SpawnLocation.X, SpawnLocation.Y, SpawnLocation.Z);
+            ABaseEnemy *NewCreature = GetWorld()->SpawnActor<ABaseEnemy>( baseEnemy, SpawnLocation, FRotator::ZeroRotator);
+            UE_LOG(LogTemp, Warning, TEXT("Creature spawned at X: %f, Y: %f, Z: %f"), SpawnLocation.X, SpawnLocation.Y, SpawnLocation.Z);
          }
       else
          {
@@ -98,4 +85,5 @@ AFighterGameGameMode::Spawn()
          }
    }
 }
+
 
